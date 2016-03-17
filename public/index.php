567f2c5e6e845178b3ea40491f8b373fdc1e122e
Ben Bankes <ben@sbninc.com>
2016-03-16 21:01:17 -0600

    Add category table

--- public/index.php
+++ public/index.php
@@ -2,6 +2,23 @@
 require_once '../vendor/autoload.php';
 require_once '../generated-conf/config.php';
 
-use \Uphpu\Propel2Quickstart\SupplierQuery;
+use \Uphpu\Propel2Quickstart\JobQuery;
+use \Uphpu\Propel2Quickstart\Job;
+use \Propel\Runtime\Propel;
+Propel::getConnection()->useDebug(true);
 
-print_r(SupplierQuery::create()->find()->toArray());
+$text = print_r(JobQuery::create()->find()->toArray(), true);
+
+Propel::getConnection()->useDebug(true);
+
+$colJobs = JobQuery::create()->find();
+$colJobs->populateRelation('SupplierProductJobs');
+foreach ($colJobs as $aJob) {
+    $aJob->getSupplierProductJobs();
+}
+
+echo Propel::getConnection()
+    ->getLastExecutedQuery();
+
+?>
+<pre><?=$text?></pre>
